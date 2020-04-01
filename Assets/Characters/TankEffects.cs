90114039671bb1845c663b4592e970b8d87f649f
Scaelz <avchursins@gmail.com>
2020-04-01 17:35:19 +0300

    Подключен эффект взрыва при убийстве вражесского танка

--- Assets/Characters/TankEffects.cs
+++ Assets/Characters/TankEffects.cs
@@ -2,8 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(TankHealth))]
 public class TankEffects : MonoBehaviour
 {
     [Header("Visual effects")]
-    GameObject[] explosions;
+    [SerializeField] GameObject[] explosions;
+    TankHealth tankHealth;
+
+    private void Start()
+    {
+        tankHealth = GetComponent<TankHealth>();
+        tankHealth.OnGotKilled += PlayRandomExplosion;
+    }
+
+    void PlayRandomExplosion()
+    {
+        Vector3 point = transform.position;
+        GameObject prefab = explosions[Random.Range(0, explosions.Length)];
+        ParticleSystem ps = Instantiate(prefab, point, Quaternion.identity).GetComponent<ParticleSystem>();
+        ps.Play();
+        Destroy(ps.gameObject, 5);
+    }
+
 }
