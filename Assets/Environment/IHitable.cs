8fa9f5c44b3f519e8a1bbc1b66db76272aaed046
QQmber <theqqmber@gmail.com>
2020-04-04 20:34:19 +0300

    Добавлены здания и стены
    Созданые префабы здания, добавлены материалы.

--- Assets/Environment/IHitable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-public interface IHitable
-{
-    event Action OnGotHit;
-    void Hit(List<Collider> colliders, Vector3 hitDirection, Vector3 hitPoint);
-}
